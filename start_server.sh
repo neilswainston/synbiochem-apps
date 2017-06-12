2c43e6820bd2bcebf82ad1717d981fc3cd08b1e5
Neil Swainston <neil.swainston@manchester.ac.uk>
2017-06-12 16:34:24 +0100

    Added DEbrief application.

--- start_server.sh
+++ start_server.sh
@@ -9,6 +9,12 @@ cd CodonGenie
 docker build -t codongenie .
 cd ..
 
+git clone https://github.com/synbiochem/DEbrief.git
+cd DEbrief
+docker build -t debrief .
+cd ..
+
 docker run --name nginx-proxy -d -p 80:80 -v /var/run/docker.sock:/tmp/docker.sock:ro jwilder/nginx-proxy
 docker run --name partsgenie -d -p :5000 -e VIRTUAL_HOST=parts.synbiochem.co.uk partsgenie
 docker run --name codongenie -d -p :5000 -e VIRTUAL_HOST=codon.synbiochem.co.uk codongenie
+docker run --name debrief -d -p :5000 -e VIRTUAL_HOST=debrief.synbiochem.co.uk debrief
