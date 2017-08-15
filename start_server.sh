03a93a922cc29aa74f47e15e50b32f9bb5a7cd49
Neil Swainston <neil.swainston@manchester.ac.uk>
2017-08-15 16:31:34 +0100

    Updated for PartsGenie.

--- start_server.sh
+++ start_server.sh
@@ -6,7 +6,7 @@ docker rmi $(docker images -q)
 rm -rf PathwayGenie
 git clone https://github.com/synbiochem/PathwayGenie.git
 cd PathwayGenie
-docker build -t pathwaygenie .
+docker build -t partsgenie .
 cd ..
 
 rm -rf CodonGenie
@@ -22,6 +22,6 @@ docker build -t debrief .
 cd ..
 
 docker run --name nginx-proxy -d -p 80:80 -v /var/run/docker.sock:/tmp/docker.sock:ro jwilder/nginx-proxy
-docker run --name pathwaygenie -d -p :5000 -e VIRTUAL_HOST=pathway.synbiochem.co.uk pathwaygenie
+docker run --name partsgenie -d -p :5000 -e VIRTUAL_HOST=parts.synbiochem.co.uk pathwaygenie
 docker run --name codongenie -d -p :5000 -e VIRTUAL_HOST=codon.synbiochem.co.uk codongenie
 docker run --name debrief -d -p :5000 -e VIRTUAL_HOST=debrief.synbiochem.co.uk debrief
