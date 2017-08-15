bc3058ae506ba6a63abaae8aa7e44faf7b23a548
Neil Swainston <neil.swainston@manchester.ac.uk>
2017-08-15 16:45:13 +0100

    Corrected error in start_server script.

--- start_server.sh
+++ start_server.sh
@@ -22,6 +22,6 @@ docker build -t debrief .
 cd ..
 
 docker run --name nginx-proxy -d -p 80:80 -v /var/run/docker.sock:/tmp/docker.sock:ro jwilder/nginx-proxy
-docker run --name partsgenie -d -p :5000 -e VIRTUAL_HOST=parts.synbiochem.co.uk pathwaygenie
+docker run --name partsgenie -d -p :5000 -e VIRTUAL_HOST=parts.synbiochem.co.uk partsgenie
 docker run --name codongenie -d -p :5000 -e VIRTUAL_HOST=codon.synbiochem.co.uk codongenie
 docker run --name debrief -d -p :5000 -e VIRTUAL_HOST=debrief.synbiochem.co.uk debrief
