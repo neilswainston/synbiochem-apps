e8b727eac341a248380f3e2a63a4da74c8b5ac1a
Neil Swainston <neil.swainston@manchester.ac.uk>
2017-11-15 12:23:21 +0000

    Added PathwayGenie 2.

--- start_server.sh
+++ start_server.sh
@@ -6,7 +6,13 @@ docker rmi $(docker images -q)
 rm -rf PathwayGenie
 git clone https://github.com/synbiochem/PathwayGenie.git
 cd PathwayGenie
-docker build -t partsgenie .
+docker build -t pathwaygenie .
+cd ..
+
+rm -rf PathwayGenie2
+git clone https://github.com/genegeniebio/PathwayGenie.git PathwayGenie2
+cd PathwayGenie2
+docker build -t pathwaygenie2 .
 cd ..
 
 rm -rf CodonGenie
@@ -22,6 +28,7 @@ docker build -t debrief .
 cd ..
 
 docker run --name nginx-proxy -d -p 80:80 -v /var/run/docker.sock:/tmp/docker.sock:ro jwilder/nginx-proxy
-docker run --name partsgenie -d -p :5000 -e VIRTUAL_HOST=parts.synbiochem.co.uk partsgenie
+docker run --name pathwaygenie -d -p :5000 -e VIRTUAL_HOST=parts.synbiochem.co.uk pathwaygenie
+docker run --name pathwaygenie2 -d -p :5000 -e VIRTUAL_HOST=pathway.synbiochem.co.uk pathwaygenie2
 docker run --name codongenie -d -p :5000 -e VIRTUAL_HOST=codon.synbiochem.co.uk codongenie
 docker run --name debrief -d -p :5000 -e VIRTUAL_HOST=debrief.synbiochem.co.uk debrief
